edf28203f0bc75933428b40dffbc07712ecb6093
kathiravan292 <kkathiravan785@gmail.com>
2025-11-15 16:54:32 +0530

    link

--- blogapp.js
+++ blogapp.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://blogclient-chi.vercel.app/"],
     credentials: true,
     optionsSuccessStatus: 200
 }));
